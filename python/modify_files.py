622532177870da11b4522a0b8ea5fd1103aea8c5
Yas <yasisay@gmail.com>
2024-06-26 23:13:33 -0700

    Update modify_files.py to copy subfolders

--- python/modify_files.py
+++ python/modify_files.py
@@ -13,9 +13,9 @@ if not os.path.exists(src_dir):
 
 # Iterate over all files in the out directory
 for filename in os.listdir(out_dir):
-    if filename.endswith('.html'):
-        filepath = os.path.join(out_dir, filename)
-        
+    filepath = os.path.join(out_dir, filename)
+    
+    if os.path.isfile(filepath) and filename.endswith('.html'):
         # Read the file content
         with open(filepath, 'r', encoding='utf-8') as file:
             content = file.read()
@@ -26,16 +26,27 @@ for filename in os.listdir(out_dir):
         # Make modifications to the HTML content
         # For example, adding a new paragraph
         new_paragraph = soup.new_tag('p')
-        new_paragraph.string = "This is an awesome new paragraph added by Beautiful Soup."
+        new_paragraph.string = "This is a new paragraph added by Beautiful Soup."
         soup.body.append(new_paragraph)
         
         # Write the modified content back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
             file.write(str(soup))
 
-# Copy all files except *.css to ../src
-for filename in os.listdir(out_dir):
-    if not filename.endswith('.css'):
-        source_path = os.path.join(out_dir, filename)
-        destination_path = os.path.join(src_dir, filename)
-        shutil.copy2(source_path, destination_path)
+# Function to copy files and directories excluding .css files
+def copy_files(src, dst):
+    if os.path.isdir(src):
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+        for item in os.listdir(src):
+            s = os.path.join(src, item)
+            d = os.path.join(dst, item)
+            if os.path.isdir(s):
+                copy_files(s, d)
+            elif not s.endswith('.css'):
+                shutil.copy2(s, d)
+    elif not src.endswith('.css'):
+        shutil.copy2(src, dst)
+
+# Copy all files and directories except *.css to ../src
+copy_files(out_dir, src_dir)
