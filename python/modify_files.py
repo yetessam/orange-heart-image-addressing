692a189c2aaba25d9af3c12848aa8b482bee75b3
Yas <yasisay@gmail.com>
2024-06-26 23:08:21 -0700

    Update modify_files.py to copy files to src

--- python/modify_files.py
+++ python/modify_files.py
@@ -1,9 +1,15 @@
-# modify_files.py, written by Chat GPT
+# modify_files.py, written by ChatGPT
 from bs4 import BeautifulSoup
 import os
+import shutil
 
 # Directory containing the files to modify
 out_dir = 'out'
+src_dir = '../src'
+
+# Create the src directory if it doesn't exist
+if not os.path.exists(src_dir):
+    os.makedirs(src_dir)
 
 # Iterate over all files in the out directory
 for filename in os.listdir(out_dir):
@@ -20,9 +26,16 @@ for filename in os.listdir(out_dir):
         # Make modifications to the HTML content
         # For example, adding a new paragraph
         new_paragraph = soup.new_tag('p')
-        new_paragraph.string = "This is a new paragraph added by Beautiful Soup."
+        new_paragraph.string = "This is an awesome new paragraph added by Beautiful Soup."
         soup.body.append(new_paragraph)
         
         # Write the modified content back to the file
         with open(filepath, 'w', encoding='utf-8') as file:
             file.write(str(soup))
+
+# Copy all files except *.css to ../src
+for filename in os.listdir(out_dir):
+    if not filename.endswith('.css'):
+        source_path = os.path.join(out_dir, filename)
+        destination_path = os.path.join(src_dir, filename)
+        shutil.copy2(source_path, destination_path)
