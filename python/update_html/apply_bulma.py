d6e10efe77386b64e4c40b874bbcb34b9e9813cb
Yas <yasisay@gmail.com>
2024-07-12 09:14:11 -0700

    Update apply_bulma.py

--- python/update_html/apply_bulma.py
+++ python/update_html/apply_bulma.py
@@ -3,17 +3,18 @@ from update_html.bulma_classes import bulma_classes
 
 def apply_bulma_classes(soup):
     for tag, class_attr in bulma_classes.items():
-        for element in soup.find_all(tag):
+        elements = soup.select(selector)
+        for element in elements:
             if 'class' not in element.attrs:
                 # If the element does not have a class attribute, add one
                 element['class'] = [class_attr]
-                print(f"Applied Bulma class '{class_attr}' to <{tag}> tag without existing classes.")
+                print(f"Applied Bulma class '{class_attr}' to <{element.name}> tag without existing classes.")
             elif class_attr not in element['class']:
                 # If the element has a class attribute but does not contain the class_attr, add it
                 element['class'].append(class_attr)
-                print(f"Added Bulma class '{class_attr}' to existing classes of <{tag}> tag.")
+                print(f"Added Bulma class '{class_attr}' to existing classes of <{element.name}> tag.")
             else:
                 # If the element already has the class_attr, skip
-                print(f"Skipped <{tag}> tag as it already contains the Bulma class '{class_attr}'.")
-
+                print(f"Skipped <{element.name}> tag as it already contains the Bulma class '{class_attr}'.")
+                
     return soup
