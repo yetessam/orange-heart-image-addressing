da48bf6a3c8692c46c244fe68f43b51351392206
Yas <yasisay@gmail.com>
2024-08-03 11:26:43 -0700

    Update create_pictures.py

--- python/update_html/create_pictures.py
+++ python/update_html/create_pictures.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-from logging_config import logger
+from logging import logger
 
 def create_picture_tags(soup, filepath):
     figgroup_divs = soup.find_all('div', class_=lambda c: c and 'figgroup' in c.split() and 'picture' in c.split())
