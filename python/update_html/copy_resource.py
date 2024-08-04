0823d85bc8b10c429cf1236fcff33f5f8bc6b646
Yas <yasisay@gmail.com>
2024-08-03 17:06:14 -0700

    Update copy_resource.py

--- python/update_html/copy_resource.py
+++ python/update_html/copy_resource.py
@@ -3,18 +3,17 @@ import shutil
 
 from update_html.logging_ohp import logger
 
-def copy_resource(resource_dir, src_dir, filename):
-    source_file_path = os.path.join(resource_dir, filename)
-    destination_dir = os.path.join(src_dir, "css")
-    destination_file_path = os.path.join(destination_dir, filename)
+def copy_resource(source_dir, dest_dir, filename):
+   source_file_path = os.path.join(source_dir, filename)
+   destination_file_path = os.path.join(dest_dir, filename)
 
     if not os.path.exists(source_file_path):
         logger.info(f"File not found in resource directory: {source_file_path}")
         return
 
-    if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
-        logger.info(f"Created directory: {destination_dir}")
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+        logger.info(f"Created directory: {dest_dir}")
 
     shutil.copy2(source_file_path, destination_file_path)
     logger.info(f"Copied {source_file_path} to {destination_file_path}")
