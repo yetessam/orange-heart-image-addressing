346ffb119154c4d650b4243b3f80572e02f62001
Yas <yasisay@gmail.com>
2025-01-20 09:42:23 -0800

    Update modify_navbar.py

--- python/update_html/modify_navbar.py
+++ python/update_html/modify_navbar.py
@@ -53,7 +53,6 @@ def modify_navbar(soup):
         ul = nav.find('ul')
         if ul:
             set_navbar_classes(ul);
-        
             navbar_start.append(ul);
             logger.debug("Reorganized <nav> structure with Bulma classes.")
 
@@ -63,55 +62,9 @@ def modify_navbar(soup):
         nav.clear()
         nav.append(navbar_brand)
         nav.append(navbar_menu)
-
-        # Add inline script for Bulma navbar functionality
-        script_content = """
-
-      document.addEventListener('DOMContentLoaded', () => {
-            const navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-            const targetMenu = document.getElementById('navbarBasicExample');
-
-            const isDesktop = () => window.innerWidth >= 1024; // Adjust this breakpoint as needed
-
-            const setInitialMenuState = () => {
-                if (isDesktop() && localStorage.getItem('navbar-menu-state') === 'active') {
-                    targetMenu.classList.add('is-active');
-                    navbarBurgers.forEach(el => el.classList.add('is-active'));
-                } else {
-                    targetMenu.classList.remove('is-active');
-                    navbarBurgers.forEach(el => el.classList.remove('is-active'));
-                }
-            };
-
-            setInitialMenuState();
-
-            // Add click event to toggle state
-            navbarBurgers.forEach(el => {
-                el.addEventListener('click', () => {
-                    el.classList.toggle('is-active');
-                    targetMenu.classList.toggle('is-active');
-
-                    // Save the state to localStorage if on desktop
-                    if (isDesktop()) {
-                        if (targetMenu.classList.contains('is-active')) {
-                            localStorage.setItem('navbar-menu-state', 'active');
-                        } else {
-                            localStorage.removeItem('navbar-menu-state');
-                        }
-                    }
-                });
-            });
-
-            // Recheck the menu state on window resize
-            window.addEventListener('resize', () => {
-                setInitialMenuState();
-            });
-        });
-  
         
-        """
-        script_tag = soup.new_tag('script', type='text/javascript')
-        script_tag.string = script_content
+        # Add javascript for Bulma
+        script_tag = soup.new_tag('script', src='js/navbar.js', type='text/javascript')
         nav.insert_after(script_tag)
 
         nav['id'] = 'navbar-bulma'
