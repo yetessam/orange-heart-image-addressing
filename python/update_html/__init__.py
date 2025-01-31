8b58a71f0f4022d2d98b81b517515d5559dbad48
Yas <yasisay@gmail.com>
2025-01-31 08:18:43 -0800

    Update __init__.py

---
+++ python/update_html/__init__.py
@@ -0,0 +1,10 @@
+# update_html/__init__.py
+
+from .apply_bulma import apply_bulma_classes
+from .bulma_classes import bulma_classes
+from .create_pictures import create_picture_tags
+from .create_responsive import create_responsive
+from .file_operations import copy_files, read_html_file, write_html_file
+from .logging_ohp import logger
+from .modify_navbar import modify_navbar
+from .update_head import update_head
